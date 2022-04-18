72c8e370e54e70725baf22e3de474234c97c4080
tharshi2803 <67555968+tharshi2803@users.noreply.github.com>
2022-04-18 23:21:12 +0530

    node modules added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Aboutus from './components/Aboutus/Aboutus';
+import Calculator from './components/Calculator/Calculator';
 import Contact from './components/Contact/Contact';
 import Services from './components/Services/Services';
 
@@ -9,7 +10,8 @@ function App() {
       <Router>
         <Aboutus />
         <Contact />
-        <Services /> 
+        <Services />
+        <Calculator /> 
       </Router>
     </div>
   );
